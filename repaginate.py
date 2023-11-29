a1196973bb76d9dbe556b1e1d67710fdfbfd072c
Jason Best <jbest@brit.org>
2023-11-29 14:13:05 -0600

    all working

--- repaginate.py
+++ repaginate.py
@@ -4,9 +4,10 @@ from pathlib import Path
 from PIL import Image
 
 
-#input_path = 'test/FieldNotebook1_(BRIT-A-AR003-FN01)_1-646/'
-input_path = 'test/small_batch/'
+input_path = 'test/FieldNotebook1_(BRIT-A-AR003-FN01)_1-646/'
+#input_path = 'test/small_batch/'
 output_path = 'test/out/'
+output_file_ext = 'jpg'
 images = glob.glob(input_path + '*.tif')
 
 # TODO - Make sure sort is correct
@@ -26,7 +27,7 @@ for image_path in images:
     image_name = image_path.stem
     page_sep = image_name.find('_')
     notebook_ID = image_name[:page_sep]
-    print(image_name, notebook_ID)
+    #print(image_name, notebook_ID)
     
     spread = Image.open(image_path)
     spread_width, height = spread.size
@@ -37,18 +38,29 @@ for image_path in images:
         page_width = page_width + (page_width *.05) # padding to crop past center of images
         left = spread.crop((0,0,page_width,height))
         #print(spread_width)
-        left.show()
-        print(str(page_counter).zfill(3))
+        #left.show()
+        image_out_path = Path(output_path)
+        image_out_name = notebook_ID + '_' + str(page_counter).zfill(3) + '.' + output_file_ext
+        image_out_path = image_out_path / image_out_name
+        print(image_out_path)
+        left.save(image_out_path)
         page_counter +=1
 
         # Crop right page
         right = spread.crop((spread_width - page_width,0,spread_width,height))
-        right.show()
-        print(str(page_counter).zfill(3))
+        #right.show()
+        image_out_path = Path(output_path)
+        image_out_name = notebook_ID + '_' + str(page_counter).zfill(3) + '.' + output_file_ext
+        image_out_path = image_out_path / image_out_name
+        print(image_out_path)
+        right.save(image_out_path)
         page_counter +=1
     else:
         #image is narrow, probably front or back cover
-        spread.show()
-        #TODO paginate and save file with new name
-        print(str(page_counter).zfill(3))
+        #spread.show()
+        image_out_path = Path(output_path)
+        image_out_name = notebook_ID + '_' + str(page_counter).zfill(3) + '.' + output_file_ext
+        image_out_path = image_out_path / image_out_name
+        print(image_out_path)
+        spread.save(image_out_path)
         page_counter +=1
