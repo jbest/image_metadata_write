a749fb5b9238e7765d548233618954240cdae259
Jason Best <jbest@brit.org>
2023-11-29 13:32:27 -0600

    first commit

--- repaginate.py
+++ repaginate.py
@@ -17,23 +17,30 @@ print(test)
 """
 
 page_counter = 0
-spread_width_min = 0
+spread_width_min = 2800 # full spreads seem to all be 2855
 for image_path in images:
-	# TODO skip first file (front cover)
-	
-	spread = Image.open(image_path)
-	spread_width, height = spread.size
-	#spread.show()
-	# Crop left page
-	page_width = math.floor(spread_width/2)
-	left = spread.crop((0,0,page_width,height))
-	print(spread_width)
-	left.show()
+    # TODO skip first file (front cover)
+    
+    spread = Image.open(image_path)
+    spread_width, height = spread.size
+    if spread_width > spread_width_min:
+        #spread.show()
+        # Crop left page
+        page_width = math.floor(spread_width/2)
+        left = spread.crop((0,0,page_width,height))
+        print(spread_width)
+        left.show()
+        print(str(page_counter).zfill(3))
+        page_counter +=1
 
-
-
-	# Crop right page
-	right = spread.crop((spread_width - page_width,0,spread_width,height))
-	right.show()
-	print(str(page_counter).zfill(3))
-	page_counter +=1
+        # Crop right page
+        right = spread.crop((spread_width - page_width,0,spread_width,height))
+        right.show()
+        print(str(page_counter).zfill(3))
+        page_counter +=1
+    else:
+        #image is narrow, probably front or back cover
+        spread.show()
+        #TODO paginate and save file with new name
+        print(str(page_counter).zfill(3))
+        page_counter +=1
